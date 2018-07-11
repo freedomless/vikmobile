9b189624bb58a02daa3d024c9ffb7e78ed4bed54
Dobromir <dobromirtodoroff@gmail.com>
2018-07-11 18:53:15 +0300

    sdf

--- platforms/browser/www/js/ajax.js
+++ platforms/browser/www/js/ajax.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
         }).done(function(dataFromServer) {
             updateElectricData(dataFromServer);
         }).fail(function () {
-            alert("fail");
             $("#debug").html(dataFromServer);
         });
     }
@@ -30,8 +29,23 @@ $(document).ready(function() {
         barPurcentage = Number(dataFromServer[10] / 2700 * 100);
         $("#utrBar").css("width", barPurcentage + "%");
         $("#utr").html(dataFromServer[10] / 10 + " V");
-
-        console.log(dataFromServer);
-        $("#debug").html(dataFromServer);
     }
+
+    function checkConnection() {
+        var networkState = navigator.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        alert('Connection type: ' + states[networkState]);
+    }
+
+    checkConnection();
 });
